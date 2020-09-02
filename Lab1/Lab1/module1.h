28e827515984de5c6f3ee30c11d51b609d01f06f
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-02 14:32:09 +0300

    Edit window

--- Lab1/Lab1/module1.h
+++ Lab1/Lab1/module1.h
@@ -6,16 +6,17 @@
 #define IDD_DIALOG1                     101
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
+#define IDC_NEXT_MOD1                   1003
+#define IDC_CANCEL_MOD1                 1004
 
+extern INT_PTR CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 // Next default values for new objects
 // 
-extern INT_PTR CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
-
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

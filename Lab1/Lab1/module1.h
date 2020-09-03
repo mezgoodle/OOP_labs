2335a93e01244023a4f9942c595dd1106f64c28d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-03 17:30:50 +0300

    Some updates

--- Lab1/Lab1/module1.h
+++ Lab1/Lab1/module1.h
@@ -6,23 +6,24 @@
 #define IDD_WORK                        101
 #define IDD_WORK_MOD1                   101
 #define IDD_DIALOG2                     105
-#define IDD_WORK_MOD2                   105
+#define IDD_WORK_MOD2                   108
 #define IDC_NEXT_MOD1                   1001
 #define IDC_CANCEL_MOD1                 1002
-#define IDC_OK_MOD2                     1008
-#define IDC_CANCEL_MOD2                 1009
-#define IDC_BACK_MOD2                   1010
-
-extern BOOL CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
-extern BOOL CALLBACK Work_MOD2(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+#define IDC_BUTTON1                     1011
+#define IDC_OK_MOD2                     1012
+#define IDC_CANCEL_MOD2                 1013
+#define IDC_BACK_MOD2                   1014
 
 // Next default values for new objects
 // 
+extern BOOL CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+extern BOOL CALLBACK Work_MOD2(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

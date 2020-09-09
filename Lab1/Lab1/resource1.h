be4195ed2bf62ed848545ac21492e29524e24ff5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-09 21:07:30 +0300

    Create files

---
+++ Lab1/Lab1/resource1.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// An include file created in Microsoft Visual C ++.
+// Used by module1.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_WORK_MOD1                   101
+#define IDD_DIALOG2                     105
+#define IDD_WORK_MOD2                   108
+#define IDC_NEXT_MOD1                   1001
+#define IDC_CANCEL_MOD1                 1002
+#define IDC_BUTTON1                     1011
+#define IDC_OK_MOD2                     1012
+#define IDC_CANCEL_MOD2                 1013
+#define IDC_BACK_MOD2                   1014
+
+// Next default values for new objects
+// 
+extern BOOL CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+extern BOOL CALLBACK Work_MOD2(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

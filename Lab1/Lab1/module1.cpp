d4dc9016a648ec50246a16771fb5ca0e8954f238
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-10 18:55:40 +0300

    Update module1

--- Lab1/Lab1/module1.cpp
+++ Lab1/Lab1/module1.cpp
@@ -11,6 +11,11 @@ int Func_MOD1(HINSTANCE hInst, HWND hWnd)
     return DialogBox(hInst, MAKEINTRESOURCE(IDD_WORK_MOD1), hWnd, Work_MOD1);
 }
 
+BOOL onClick(HWND hDlg, WPARAM wParam)
+{
+    return EndDialog(hDlg, LOWORD(wParam));
+}
+
 // First callback
 BOOL CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -21,19 +26,17 @@ BOOL CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
             switch(LOWORD(wParam)) 
             {
                 case IDC_CANCEL_MOD1:
-                    EndDialog(hDlg, LOWORD(wParam));
+                    onClick(hDlg, wParam);
                     DefWindowProc(hDlg, message, wParam, lParam);
                     return TRUE;
                 case IDC_NEXT_MOD1:
-                    EndDialog(hDlg, LOWORD(wParam));
+                    onClick(hDlg, wParam);
                     Func_MOD3(hinst1, hDlg);
                     return TRUE;
             }
             break;
         case WM_CLOSE:
-        {
             EndDialog(hDlg, 0);
-        }
         break;
     }
     return FALSE;
--- Lab1/Lab1/module1.h
+++ Lab1/Lab1/module1.h
@@ -1,5 +1,3 @@
 #pragma once
 #include "resource1.h"
 extern int Func_MOD1(HINSTANCE hInst, HWND hWnd);
-// extern BOOL CALLBACK Work_MOD1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
-

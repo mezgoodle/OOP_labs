822fc5bd37d03a1eec3bc86bc538302896de41bd
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-20 16:38:10 +0300

    Some updates

--- Lab2/Lab2/editor.h
+++ Lab2/Lab2/editor.h
@@ -1,1 +1,9 @@
 #pragma once
+class Editor {
+	public: //а, можливо, ще потрібен віртуальний деструктор?
+		virtual void OnLBdown(HWND) = 0;
+		virtual void OnLBup(HWND) = 0;
+		virtual void OnMouseMove(HWND) = 0;
+		virtual void OnPaint(HWND) = 0;
+	};
+};

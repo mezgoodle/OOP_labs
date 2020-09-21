688157765d72c3282550d040b0f45ebcb326d8bc
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-21 12:51:07 +0300

    Update files

--- Lab2/Lab2/shape.h
+++ Lab2/Lab2/shape.h
@@ -1,19 +1,31 @@
-#pragma once
-class Shape
-{
-	protected:
-		long xs1, ys1, xs2, ys2;
-	public:
-		void Set(long x1, long y1, long x2, long y2);
-		virtual void Show(HDC) = 0;
-	};
+class Shape {
+protected:
+	long xs1, ys1, xs2, ys2;
+	COLORREF color;
+	BOOL isFill;
+public:
+	void Set(long, long, long, long);
+	void SetColor(COLORREF);
+	void SetFill(BOOL);
+	virtual void Show(HDC) = 0;
 };
 
-//---визначення методу Set---
-void Shape::Set(long x1, long y1, long x2, long y2)
+class PointShape : public Shape
 {
-	xs1 = x1;
-	ys1 = y1;
-	xs2 = x2;
-	ys2 = y2;
-}
+	void Show(HDC);
+};
+
+class LineShape : public Shape
+{
+	void Show(HDC);
+};
+
+class RectShape : public Shape
+{
+	void Show(HDC);
+};
+
+class EllipseShape : public Shape
+{
+	void Show(HDC);
+};
--- Lab2/Lab2/shape_editor.h
+++ Lab2/Lab2/shape_editor.h
@@ -1,18 +1,69 @@
 #pragma once
-#include "editor.h";
-class ShapeObjectsEditor: public Editor {
-	private:
+#include "editor.h"
+#include "shape.h"
 
-	public:
-		ShapeObjectsEditor(void);
-		~ShapeObjectsEditor();
-		void StartPointEditor(...);
-		void StartLineEditor(...);
-		void StartRectEditor(...);
-		void StartEllipseEditor(...);
-		void OnLBdown(HWND);
-		void OnLBup(HWND);
-		void OnMouseMove(HWND);
-		void OnPaint(HWND);
-		void OnInitMenuPopup(HWND, WPARAM); //відповідно варіанту завдання
+class ShapeEditor : public Editor
+{
+protected:
+	int x1, y1, x2, y2;
+public:
+	void OnLBdown(HWND);
+	void OnLBup(HWND, COLORREF, BOOL);
+	void OnMouseMove(HWND);
+	void OnPaint(HWND);
+	virtual void OnInitMenuPopup(HWND, WPARAM);
+};
+
+class ShapeObjectsEditor {
+protected:
+	ShapeEditor* pse;
+public:
+	ShapeObjectsEditor();
+	~ShapeObjectsEditor();
+	void StartPointEditor();
+	void StartLineEditor();
+	void StartRectEditor();
+	void StartEllipseEditor();
+	void OnLBdown(HWND);
+	void OnLBup(HWND, COLORREF, BOOL);
+	void OnMouseMove(HWND);
+	void OnPaint(HWND);
+	void OnInitMenuPopup(HWND, WPARAM);
+};
+
+
+
+class PointEditor : public ShapeEditor
+{
+public:
+	void OnLBdown(HWND);
+	void OnLBup(HWND, COLORREF, BOOL);
+	void OnInitMenuPopup(HWND, WPARAM) override;
+};
+
+class LineEditor : public ShapeEditor
+{
+public:
+	void OnLBdown(HWND);
+	void OnLBup(HWND, COLORREF, BOOL);
+	void OnMouseMove(HWND);
+	void OnInitMenuPopup(HWND, WPARAM);
+};
+
+class RectEditor : public ShapeEditor
+{
+public:
+	void OnLBdown(HWND);
+	void OnLBup(HWND, COLORREF, BOOL);
+	void OnMouseMove(HWND);
+	void OnInitMenuPopup(HWND, WPARAM);
+};
+
+class EllipseEditor : public ShapeEditor
+{
+public:
+	void OnLBdown(HWND);
+	void OnLBup(HWND, COLORREF, BOOL);
+	void OnMouseMove(HWND);
+	void OnInitMenuPopup(HWND, WPARAM);
 };
